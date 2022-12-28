25188c91d02232921187c27b457acf3a3dad170a
maratahmetov <ax-marat@mail.ru>
2022-12-28 23:28:08 +0300

    f

--- store/forms.py
+++
@@ -1,8 +0,0 @@
-from .models.product import Products
-from django import forms
-
-
-class ProductCreate(forms.ModelForm):
-    class Meta:
-        model = Products
-        fields = '__all__'
--- store/urls.py
+++ store/urls.py
@@ -1,26 +1,28 @@
-from django.contrib import admin
-from django.urls import path
-from .views.home import Index , store
-from .views.signup import Signup
-from .views.login import Login , logout
-from .views.cart import Cart
-from .views.checkout import CheckOut
-from .views.orders import OrderView
-from .views.upload import upload_product
-from .middlewares.auth import  auth_middleware
-from . import views
+"""store URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', Index.as_view(), name='homepage'),
-    path('store', store , name='store'),
-
-    path('signup', Signup.as_view(), name='signup'),
-    path('login', Login.as_view(), name='login'),
-    path('upload', views.upload.upload_product, name='upload'),
-    path('logout', logout , name='logout'),
-    path('cart', auth_middleware(Cart.as_view()) , name='cart'),
-    path('check-out', CheckOut.as_view() , name='checkout'),
-    path('orders', auth_middleware(OrderView.as_view()), name='orders'),
-
+    path('admin/', admin.site.urls),
+    path('store/', include('products.urls')),
+    path('users/', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
